f9dd8746746f005b458f9c4f0a76ec19a78c799a
Eitan-Chertok <eitan.chertok@ibm.com>
2022-06-08 19:03:32 +0300

    added search recipe

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const DButils = require("./DButils");
+const dbUtils = require("./DButils")
 const api_domain = "https://api.spoonacular.com/recipes";
 
 
@@ -10,12 +10,11 @@ const api_domain = "https://api.spoonacular.com/recipes";
  */
 
 
-
-async function getRecipeInformation() {
-    return await axios.get(`${api_domain}/random`, {
+async function getRecipeInformation(recipe_id) {
+    return await axios.get(`${api_domain}/${recipe_id}/information`, {
         params: {
             includeNutrition: false,
-            apiKey: process.env.api_token
+            apiKey: process.env.spooncular_apiKey
         }
     });
 }
@@ -39,24 +38,58 @@ async function getRecipeDetails(recipe_id) {
     }
 }
 
-async function addRecepie(recipe_details){
-    n = recipe_details.name
-    timeToMake = recipe_details.timeToMake
-    whoCanEatVegOrNot = recipe_details.whoCanEatVegOrNot,
-    glutenFree = recipe_details.glutenFree,
-    ingridients = recipe_details.ingridients,
-    instructions = recipe_details.instructions,
-    numberOfMeals = recipe_details.numberOfMeals
-    return await DButils.execQuery(`insert into recipes values ('${n}','${timeToMake}', '${whoCanEatVegOrNot}', '${glutenFree}', '${ingridients}','${instructions}','${numberOfMeals}')`);
-    // return "success"
-
+async function getRecipesByName(recipeSearchName, numberOfRecipes) {
+    let allResults = (await axios.get(`${api_domain}/complexSearch`, {
+        params: {
+            query: recipeSearchName,
+            apiKey: process.env.spooncular_apiKey,
+            number: numberOfRecipes,
+            addRecipeInformation: true
+        }
+    })).data["results"];
+    return allResults.map(function(recipe) {
+        let whoCanEat;
+        if (recipe["vegan"])
+            whoCanEat="vegan";
+        else if (recipe["vegetarian"])
+            whoCanEat="vegetarian"
+        else
+            whoCanEat="meat"
+        return {
+            "id": recipe["id"],
+            "name": recipe["title"],
+            "timeToMake": recipe["readyInMinutes"],
+            "popularity": recipe["aggregateLikes"],
+            "whoCanEatVegOrNot": whoCanEat,
+            "glutenFree": recipe["glutenFree"],
+            "image": recipe["image"],
+            "wasWatchedByUserBefore": true, //need to change here
+            "wasSavedByUser": true // need to change here
+        }
+    });
 }
 
+async function getUserRecipes(user_name, query_type) {
+    switch (query_type) {
+        case "lastWatched":
+            return await lastWatchedRecipes(user_name);
+        case "favorite":
+            return await favoriteRecipes(user_name);
+        case "created":
+            return await getRecipesCreatedByUser(user_name);
+        default:
+            throw { status: 402, message: "Invalid type of operation." };
+    }
+}
 
+async function lastWatched(user_name) {
+    let recipeIDS = await dbUtils.execQuery(
+        `SELECT TOP 3 recipeID from watchedRecipes WHERE username = '${user_name}' ORDER BY watchTime DESC`
+    )
+}
 
-
+exports.getRecipesByName = getRecipesByName;
 exports.getRecipeDetails = getRecipeDetails;
-exports.addRecepie = addRecepie;
 
 
 
