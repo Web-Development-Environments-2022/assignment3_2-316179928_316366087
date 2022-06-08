86572c537d3dded1d3c83e49f808aeaed1507037
Eitan-Chertok <eitan.chertok@ibm.com>
2022-06-08 19:17:35 +0300

    a

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const recipes_utils = require("./utils/recipes_utils");
-const DButils = require("../routes/utils/DButils");
+
 router.get("/", (req, res) => res.send("im here"));
 
 
@@ -9,7 +9,7 @@ router.get("/", (req, res) => res.send("im here"));
  * This path returns a full details of a recipe by its id
  */
 router.get("/:recipeId", async (req, res, next) => {
-  try {
+  try { 
     const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
     res.send(recipe);
   } catch (error) {
@@ -17,35 +17,32 @@ router.get("/:recipeId", async (req, res, next) => {
   }
 });
 
-
-
-
 router.post('/recipe', async (req,res,next) => {
-  let username = "ehud1"
-  try{
-    let recipe_details = {
-      name: req.body.name,
-      timeToMake: req.body.timeToMake,
-      whoCanEatVegOrNot: req.body.whoCanEatVegOrNot,
-      glutenFree: req.body.glutenFree,
-      ingridients: req.body.ingridients,
-      instructions: req.body.instructions,
-      numberOfMeals: req.body.numberOfMeals
-    }
-    recipes_from_db = await DButils.existsInDB("name", "recipes",username);
-    for (rec in recipes_from_db){
-      if (rec.name === recipe_details.name){
-        throw { status: 409, message: "recipe exists in db, please insert another name" };
+    let username = "ehud1"
+    try{
+      let recipe_details = {
+        name: req.body.name,
+        timeToMake: req.body.timeToMake,
+        whoCanEatVegOrNot: req.body.whoCanEatVegOrNot,
+        glutenFree: req.body.glutenFree,
+        ingridients: req.body.ingridients,
+        instructions: req.body.instructions,
+        numberOfMeals: req.body.numberOfMeals
       }
+      recipes_from_db = await DButils.existsInDB("name", "recipes",username);
+      for (rec in recipes_from_db){
+        if (rec.name === recipe_details.name){
+          throw { status: 409, message: "recipe exists in db, please insert another name" };
+        }
+      }
+      result = await recipes_utils.addRecepie(recipe_details);
+      res.status(200).send("The Recipe successfully added to DB");
     }
-    result = await recipes_utils.addRecepie(recipe_details);
-    res.status(200).send("The Recipe successfully added to DB");
-  }
-  catch (error){
-    next(error)  
-  }
+    catch (error){
+      next(error)  
+    }
+    
   
-
-});
+  });
 
 module.exports = router;
